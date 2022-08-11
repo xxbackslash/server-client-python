08361d9c579c9e8e39e443b0a600ef9b059fafad
xxbackslash <92121627+xxbackslash@users.noreply.github.com>
2022-08-11 00:05:09 -0400

    Update datetime_helpers.py

--- tableauserverclient/datetime_helpers.py
+++ tableauserverclient/datetime_helpers.py
@@ -19,23 +19,6 @@ class UTC(datetime.tzinfo):
     def dst(self, dt):
         return ZERO
 
-
-utc = UTC()
-TABLEAU_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+   
 
 
-def parse_datetime(date):
-    if date is None:
-        return None
-
-    try:
-        return datetime.datetime.strptime(date, TABLEAU_DATE_FORMAT).replace(tzinfo=utc)
-    except ValueError:
-        return None
-
-
-def format_datetime(date):
-    if date is None:
-        return None
-
-    return date.astimezone(tz=utc).strftime(TABLEAU_DATE_FORMAT)
